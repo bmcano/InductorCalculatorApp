e3ce967b0f3512d5d9a524cd0758ff62fc92fce1
Brandon Cano <brandoncano01@gmail.com>
2024-08-07 16:07:58 -0500

    prepare for 1.0.2 update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.brandoncano.inductancecalculator"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2 // update to 3 once update is ready
-        versionName = "1.0.2-develop"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
